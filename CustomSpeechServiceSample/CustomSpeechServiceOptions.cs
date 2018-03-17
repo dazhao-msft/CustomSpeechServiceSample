fae8340c94cdcebc20932bfff9cc8a3896b68112
David Zhao (DEVDIV) <dazhao@microsoft.com>
2018-03-17 01:48:27 +0000

    init checkin

---
+++ CustomSpeechServiceSample/CustomSpeechServiceOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomSpeechServiceSample
+{
+    public class CustomSpeechServiceOptions
+    {
+        public string AudioFile { get; set; }
+        public string[] Endpoints { get; set; }
+
+        public string PrimaryKey { get; set; }
+
+        public string SecondaryKey { get; set; }
+    }
+}
---
+++ CustomSpeechServiceSample/Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.CognitiveServices.SpeechRecognition;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CustomSpeechServiceSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                   .AddEnvironmentVariables();
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var options = configuration.Get<CustomSpeechServiceOptions>();
+
+            Task.Run(() => RunAsync(options)).GetAwaiter().GetResult();
+        }
+
+        private static async Task RunAsync(CustomSpeechServiceOptions options)
+        {
+            for (int index = 0; index < options.Endpoints.Length; index++)
+            {
+                await SendAudioAsync(options, index);
+            }
+        }
+
+        private static Task<RecognitionResult> SendAudioAsync(CustomSpeechServiceOptions options, int endpointIndex)
+        {
+            var tcs = new TaskCompletionSource<RecognitionResult>();
+
+            using (var client = SpeechRecognitionServiceFactory.CreateDataClient(SpeechRecognitionMode.ShortPhrase,
+                                                                                 "en-us",
+                                                                                 options.PrimaryKey,
+                                                                                 options.SecondaryKey,
+                                                                                 options.Endpoints[endpointIndex]))
+            {
+                client.AuthenticationUri = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken";
+
+                client.OnResponseReceived += (sender, e) =>
+                {
+                    tcs.SetResult(e.PhraseResponse);
+                };
+
+                client.OnConversationError += (sender, e) =>
+                {
+                    tcs.SetException(new InvalidOperationException(e.SpeechErrorText));
+                };
+
+                using (var audioFileStream = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), options.AudioFile), FileMode.Open, FileAccess.Read))
+                {
+                    int bytesRead = 0;
+                    byte[] buffer = new byte[1024];
+
+                    try
+                    {
+                        do
+                        {
+                            bytesRead = audioFileStream.Read(buffer, 0, buffer.Length);
+                            client.SendAudio(buffer, bytesRead);
+                        }
+                        while (bytesRead > 0);
+                    }
+                    finally
+                    {
+                        client.EndAudio();
+                    }
+                }
+
+                return tcs.Task;
+            }
+        }
+    }
+}
